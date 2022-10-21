3012fa7fdb00d3be9eb2e436f1ccb89f84642f73
KMcNickel <37396126+KMcNickel@users.noreply.github.com>
2022-10-21 13:03:34 -0700

    Add socketCAN config notes

--- src/include/socketcan.h
+++ src/include/socketcan.h
@@ -9,6 +9,41 @@
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 
+/* SocketCAN Setup
+ * For Ubuntu
+ * Reference: https://www.pragmaticlinux.com/2021/10/can-communication-on-the-raspberry-pi-with-socketcan/
+ * Reference: https://www.pragmaticlinux.com/2021/07/automatically-bring-up-a-socketcan-interface-on-boot/
+ * 
+ * Add the following to /boot/firmware/usercfg.txt
+ * dtoverlay=mcp2515-can0,oscillator=CLOCKFREQ,interrupt=25,spimaxfrequency=2000000
+ * Change CLOCKFREQ to the frequency of the MCP2515 clock
+ * Ensure dtparam=spi=on is in /boot/firmware/config.txt or add it to the usercfg file
+ * 
+ * Create a file: /etc/modules-load.d/can.conf and put in the following:
+ * can
+ * can_raw
+ * 
+ * run the following commands:
+ * sudo systemctl start systemd-networkd
+ * sudo systemctl enable systemd-networkd
+ * 
+ * Create a file: /etc/systemd/network/80-can.network and put in the following:
+ * [Match]
+ * Name=can0
+ * [CAN]
+ * BitRate=500000
+ * RestartSec=100ms
+ * 
+ * The following requires systemd 249 (Later than Ubuntu 20.04)
+ * Create a file: /etc/systemd/network/80-can.link and put in the following:
+ * [Match]
+ * Name=can0
+ * [Link]
+ * TransmitQueueLength=10000
+ * 
+ * Alternately, run the command: sudo ifconfig can0 txqueuelen 10000
+*/
+
 class SocketCAN
 {
     public:
